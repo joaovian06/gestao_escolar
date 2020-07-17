db6d7be22fbbacab907abb2e97b1eb319c57e864
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-17 14:05:15 -0300

    red: validate address length

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   describe 'validates' do
     describe 'name' do
-
       it 'name is mandatory' do
         is_expected.to validate_presence_of(:name)
       end
-
+    end
+    describe 'address' do
+      it 'address need to be larger than 15 caracters' do
+        expect(student.address.length).to be > 15
+      end
     end
   end
 end
