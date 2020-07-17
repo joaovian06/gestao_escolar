89072775ec258547643fe6292125982e40deeea5
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-17 10:12:44 -0300

    adds rspec to config/application.tb

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module GestaoEscolar
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
