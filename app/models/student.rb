2c75fe9b0cb1d8cd08589244a835ddf1a0405cb0
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-20 10:55:01 -0300

    green: validates email

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ApplicationRecord
   validates :name, presence: true,
                     length: { minimum: 10 }
   validates :address, presence: true
+  validates :email, presence: true
 end
