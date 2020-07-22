26f94a1cf1743987e141d9dade34f8088e235890
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-22 10:39:23 -0300

    add students controller and students_controller_spec

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'students/index'
+  get 'students/new'
+  get 'students/edit'
+  get 'students_controller/index'
+  get 'students_controller/new'
+  get 'students_controller/edit'
 end
