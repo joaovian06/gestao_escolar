46ead37c18a680da8d17d22a96ff2e48aadc3092
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-22 14:13:50 -0300

    red: index all students

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,1 +1,12 @@
-students_controller_spec.rb
+require 'rails_helper'
+
+RSpec.describe StudentsController, type: :controller do
+  describe '#index' do
+    let!(:students) { FactoryBot.create_list(:student, 10) }
+    before { get :index }
+
+    it 'return all students' do
+      expect(assigns[:students]).to match_array(students)
+    end
+  end
+end
