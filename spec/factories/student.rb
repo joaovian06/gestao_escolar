b95603253aa218acac82423ec73c54eb43797577
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-23 09:58:42 -0300

    name with middle student factory

--- spec/factories/student.rb
+++ spec/factories/student.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :student do
-    name { Faker::Name.name }
+    name { Faker::Name.name_with_middle }
     address { Faker::Address.street_address }
     email { Faker::Internet.email }
     birthday { Faker::Date.birthday(min_age: 6, max_age: 18) }
