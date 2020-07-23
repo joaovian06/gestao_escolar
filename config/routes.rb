ac15ef480bce5f3df5bad37c53c9e6c274d9964e
joaovian06 <joaovitor_vian@hotmail.com>
2020-07-23 10:41:00 -0300

    utilizando resources students

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-
-  get 'students/index'
-  get 'students/new'
-  get 'students/edit'
-  get 'students_controller/index'
-  get 'students_controller/new'
-  get 'students_controller/edit'
+  resources :students
 end
