9a6840e1c0b404b43aae20a445bfab980aebd367
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-05 09:40:09 -0300

    red: validate presence of name

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
+  let(:professor) { build(:professor) }
 
+  describe 'validates' do
+    describe 'name' do
+      it 'is mandatory' do
+        is_expected.to validates_presence_of(:name)
+      end
+    end
+  end
 end
