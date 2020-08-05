566027a193f917828eb9b424775e3762b717c6b1
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-05 09:47:41 -0300

    red: name length

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
-  let(:professor) { build(:professor) }
 
   describe 'validates' do
     describe 'name' do
+      let(:professor) { build(:professor, name: 'Pedro') }
       it { is_expected.to validate_presence_of(:name) }
+      it 'must have 10 caracters at minimum' do
+        expect(professor.name.length).to be >= 10
+      end
     end
   end
 end
