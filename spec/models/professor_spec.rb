a3cbb718d6340ae8c2abbd8dacb9ef20e98a38f6
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-05 09:56:50 -0300

    refactor: name length

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe Professor, type: :model do
       let(:professor) { build(:professor, name: 'Pedro Pedroso') }
 
       it { is_expected.to validate_presence_of(:name) }
-      it 'must have 10 caracters at minimum' do
-        is_expected.to validate_length_of(:name).is_at_least(10)
-      end
+      it { is_expected.to validate_length_of(:name).is_at_least(10) }
     end
   end
 end
