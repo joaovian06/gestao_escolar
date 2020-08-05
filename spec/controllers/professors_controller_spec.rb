b6196536bf5f2ee1c2b99ac31555559cc777ecb0
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-05 14:34:56 -0300

    green: #index

--- spec/controllers/professors_controller_spec.rb
+++ spec/controllers/professors_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ProfessorsController, type: :controller do
   describe '#index' do
-    let(:students) { build(:student, 10) }
+    let!(:professors) { create_list(:professor, 10) }
     before { get :index }
 
     it 'show all professors' do
-      expect(assigns[:students]).to match_array(students)
+      expect(assigns[:professors]).to match_array(professors)
     end
   end
 end
