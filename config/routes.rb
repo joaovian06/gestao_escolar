2ef7b765cb0260391dce20c5622b6944cc70ac6a
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-05 09:27:28 -0300

    init professors crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :professors
   resources :students
 end
---
+++ spec/factories/professors.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :professor do
+    ad { "MyString" }
+    name { "MyString" }
+    cellphone { "MyString" }
+  end
+end
