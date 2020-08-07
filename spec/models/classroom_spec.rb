ba6ce56c373539935449fbf5ffd68091787ce2e2
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-07 10:55:17 -0300

    refactor: model classroom presence of name

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe Classroom, type: :model do
   describe 'validations' do
     describe 'name' do
-      it 'is mandatory' do
-        is_expected.to validate_presence_of(:name)
-      end
+      it { is_expected.to validate_presence_of(:name) }
     end
   end
 end
