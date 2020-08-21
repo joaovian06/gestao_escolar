5c35ce63704508b165c7b2574afe5bffe0f888f8
joaovian06 <joaovitor_vian@hotmail.com>
2020-08-21 10:48:49 -0300

    refactor validates presence on enrollment model

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,13 +2,12 @@ class Enrollment < ApplicationRecord
   belongs_to :student
   belongs_to :classroom
 
-  validates :registration_num, presence: true
+  validates :registration_num,
+            :student,
+            :classroom,
+            presence: true
 
   validates :registration_num, uniqueness: true
-
-  validates :student, presence: true
-
-  validates :classroom, presence: true
 
   validates_length_of :registration_num, minimum: 10
 end
