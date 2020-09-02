c6bf64f00e802bac729cf2c8c884af2421034d61
joaovian06 <joaovitor_vian@hotmail.com>
2020-09-02 17:00:08 -0300

    refactor flash for classrooms on model and views

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Classroom, type: :model do
   end
 
   describe 'associations' do
-    it { is_expected.to have_many(:enrollments).inverse_of(:classroom) }
+    it { is_expected.to have_many(:enrollments).inverse_of(:classroom).dependent(:destroy) }
     it { is_expected.to accept_nested_attributes_for(:enrollments).allow_destroy(true) }
   end
 end
